04f5070ee614b63704586ee0b3327d88ffa49790
EdwinPhilip <p.edwin200294@gmail.com>
2024-06-26 16:53:49 +0530

    fix: repo, owner and pull num from env

--- main.go
+++ main.go
@@ -2,43 +2,39 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"os"
 	"strconv"
 
-	"github.com/machinebox/graphql"
 	"gh-pr-commenter/cmd"
 	"gh-pr-commenter/internal"
+
+	"github.com/machinebox/graphql"
 )
 
 func main() {
-	if len(os.Args) < 5 || (os.Args[1] == "exec" && len(os.Args) < 6) {
-		log.Fatalf("usage: %s <owner> <repo> <pr_number> <filename> or %s exec <owner> <repo> <pr_number> <command>", os.Args[0], os.Args[0])
+	if len(os.Args) < 2 {
+		log.Fatalf("usage: %s <command>", os.Args[1])
+	}
+
+	owner := os.Getenv("BASE_REPO_OWNER")
+	repo := os.Getenv("BASE_REPO_NAME")
+	prNumberStr := os.Getenv("PULL_NUM")
+	if owner == "" || repo == "" || prNumberStr == "" {
+		log.Fatalf("Environment variables OWNER, REPO, and PR_NUMBER must be set")
+	}
+
+	prNumber, err := strconv.Atoi(prNumberStr)
+	if err != nil {
+		log.Fatalf("Invalid PR number: %s", prNumberStr)
 	}
 
 	ctx := context.Background()
 	client := internal.NewGitHubClient(ctx)
 	graphqlClient := graphql.NewClient("https://api.github.com/graphql")
 
-	if os.Args[1] == "exec" {
-		owner := os.Args[2]
-		repo := os.Args[3]
-		prNumber, err := strconv.Atoi(os.Args[4])
-		if err != nil {
-			log.Fatalf("Invalid PR number: %s", os.Args[4])
-		}
-		command := os.Args[5:]
-		cmd.ExecuteAndComment(ctx, client, graphqlClient, owner, repo, prNumber, command)
-	} else {
-		owner := os.Args[1]
-		repo := os.Args[2]
-		prNumber, err := strconv.Atoi(os.Args[3])
-		if err != nil {
-			log.Fatalf("Invalid PR number: %s", os.Args[3])
-		}
-		filename := os.Args[4]
-
-		handler := cmd.NewCommandHandler(client, graphqlClient, owner, repo, prNumber)
-		handler.HandleCommand(ctx, filename)
-	}
+	command := os.Args[2:]
+    fmt.Println("Command: ", command)
+	cmd.ExecuteAndComment(ctx, client, graphqlClient, owner, repo, prNumber, command)
 }
