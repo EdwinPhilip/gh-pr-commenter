c9f984335218109688bf563a380173b764befbc7
EdwinPhilip <p.edwin200294@gmail.com>
2024-06-27 14:29:28 +0530

    fix: templates for trivy

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"strings"
 
 	"github.com/machinebox/graphql"
 	"gh-pr-commenter/cmd"
@@ -20,18 +21,42 @@ func main() {
 	repo := os.Getenv("BASE_REPO_NAME")
 	prNumberStr := os.Getenv("PULL_NUM")
 	if owner == "" || repo == "" || prNumberStr == "" {
-		log.Fatalf("Environment variables OWNER, REPO, and PR_NUMBER must be set")
+		log.Fatalf("Environment variables BASE_REPO_OWNER, BASE_REPO_NAME, and PULL_NUM must be set")
 	}
 
 	prNumber, err := strconv.Atoi(prNumberStr)
 	if err != nil {
 		log.Fatalf("Invalid PR number: %s", prNumberStr)
 	}
+	command := os.Args[2]
+
+	// always create template.md if it exists
+	templateFilename := "template.md"
+	err = createDefaultTemplate(templateFilename, command)
+	if err != nil {
+		log.Fatalf("Error creating default template: %v", err)
+	}
 
 	ctx := context.Background()
 	client := internal.NewGitHubClient(ctx)
 	graphqlClient := graphql.NewClient("https://api.github.com/graphql")
 
-	command := os.Args[2]
 	cmd.ExecuteAndComment(ctx, client, graphqlClient, owner, repo, prNumber, command)
+}
+
+// createDefaultTemplate creates a default template.md file with initial content
+func createDefaultTemplate(filename string, command string) error {
+    content := `
+<details><summary>Show Output</summary>
+
+`+"```"+`diff
+---OUTPUT---
+`+"```"+`
+</details>
+`
+	// if command contains trivy, use the trivy template as content
+	if strings.Contains(command, "trivy") {
+		content = `---OUTPUT---`
+	}
+    return os.WriteFile(filename, []byte(content), 0644)
 }
